a418ffa5bb536e4ce8503a6bb3af5559232c7c3d
shurona <shurona>
2021-06-06 18:01:51 +0900

    진행

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import BooleanComponent from "./03/BooleanComponent";
+
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <div><b>지루할 때 :</b><BooleanComponent bored /></div>
+        <div><b>즐거울 때 :</b><BooleanComponent /></div>
+      </div>
+    );
+  }
+} 
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import App from './App.jsx';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+// var img = document.createElement('img');
+// img.setAttribute('src',"http://www.easyspub.co.kr/images/logo_footer.png");
+// var divEl = document.createElement('div');
+// divEl.innerText = '안녕하세요';
+// var welcomeEl = document.createElement('div');
+// welcomeEl.append(img);
+// welcomeEl.append(divEl);
+
+// var root = document.getElementById('root');
+// root.append(welcomeEl);
