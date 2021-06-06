bc286dadee651cc9a5544674985120c9190b8cab
shurona <khc4669@naver.com>
2021-06-06 14:57:21 +0900

    commit something

---
+++ src/codetest/testing.js
@@ -0,0 +1,20 @@
+// const queryString = 'banana=10&apple=20&orange=30';
+// const chunk = queryString.split('&');
+
+// let result = chunk.map((chunk) => {
+//     const [key,value] = chunk.split('=');
+//     return { key, value};
+// }).reduce((a,item)=>{
+//     console.log(a);
+//     a[item.key] = item.value;
+//     return a;
+// }, {})
+
+// console.log(result);
+
+
+const a = () =>{
+    console.log("hello");
+}
+
+a();
